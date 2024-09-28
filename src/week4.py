4587e2f340ff7917db9db9d69d8cfa6746971e97
Joe Crockett <joe@crockett.net>
2024-09-28 17:05:16 +0100

    Additional Ex 1 from Week 4

--- src/week4.py
+++ src/week4.py
@@ -23,3 +23,21 @@ def binary_triangle(rows: int) -> None:
 def sum_lists(list_2d: list[list[int]]) -> list[int]:
     """Sums a list of lists"""
     return [sum(row) for row in list_2d]
+
+
+################################################################################
+# Additional Exercises
+################################################################################
+
+
+def king_and_wise_man() -> None:
+    """Runs the king and wise man exercise"""
+    RICE_WEIGHT = 0.03  # grams
+    total_rice = 0
+    for i in range(64):
+        rice = (2**i) * RICE_WEIGHT
+        total_rice += rice
+        print(f"{rice:.2f} grams for square {i+1}")
+
+    print(f"Total rice: {total_rice:.2f} grams")
+    print(f"{total_rice / 1_000_000_000_000_000:.2f} Gt (Gigatonnes) of rice")
